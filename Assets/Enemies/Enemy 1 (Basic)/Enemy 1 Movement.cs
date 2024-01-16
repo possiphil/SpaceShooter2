1d6aabe71932506eb0de82465c69228f68866354
Dan1elBrunner <mail@brunner-daniel.de>
2024-01-16 22:02:30 +0100

    Enemy1
    Enemy1 funktioniert jz

--- Assets/Enemies/Enemy 1 (Basic)/Enemy 1 Movement.cs
+++ Assets/Enemies/Enemy 1 (Basic)/Enemy 1 Movement.cs
@@ -3,32 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
 using Vector2 = System.Numerics.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class Enemy1Movement : MonoBehaviour
 {
    public Transform Player;
-   private Rigidbody2D rb;
+   private Rigidbody rb;
    private Vector3 movement;
 
    [SerializeField] private float moveSpeed = 2f;
-   [SerializeField] private float StopDistance = 5f;
+   [SerializeField] private float StopDistance = 3f;
+
+  
 
    private void Start()
    {
-       rb = this.GetComponent<Rigidbody2D>();
+       GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+       Player = playerObject.transform;
+       rb = GetComponent<Rigidbody>();
+       
    }
 
    private void Update()
    {
+     
+       
        Vector3 direction = Player.position - transform.position;
-
-       float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-       rb.rotation = angle;
        
        direction.Normalize();
        movement = direction;
+
+       float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+       Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+       
+       rb.MoveRotation(rotation);
+      
    }
 
    private void FixedUpdate()
@@ -36,16 +48,19 @@ public class Enemy1Movement : MonoBehaviour
        if ((Player.position - transform.position).magnitude <= StopDistance)
        {
            moveSpeed = 0;
+         
        }
        else
        {
-           moveSpeed = 1;
+           moveSpeed = 4;
            moveEnemy(movement);
        }
    }
 
    private void moveEnemy(Vector3 direction)
    {
+
        rb.MovePosition(transform.position + (moveSpeed * Time.deltaTime * direction));
+       
    }
 }
